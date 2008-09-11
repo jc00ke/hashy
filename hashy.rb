3d7f74bf86bdbf3dac20b364b987778e40474ce1
Jesse Cooke <jesse@chino.(none)>
2008-09-10 22:56:37 -0700

    Trying to get some type of persistence going but not having much luck.

--- hashy.rb
+++ hashy.rb
@@ -1,8 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'digest'
-#require 'digest/md5'
-#require 'digest/sha2'
+require 'singleton'
+
+class Hashy
+	include Singleton
+
+	def initialize
+		@values = {}
+	end
+
+	def self.get_hashes(text)		
+		if @values.has_key?(text)
+			@values[text]
+		else
+			md5 = Digest::MD5.hexdigest(text)
+			sha1 = Digest::SHA1.hexdigest(text)
+			sha2 = Digest::SHA256.hexdigest(text)
+			body = "
+				$('md5').value = '#{md5}';
+				$('sha1').value = '#{sha1}';
+				$('sha2').value = '#{sha2}';
+			"
+			@values[text] = body
+		end
+	end
+end
+	
+
 
 get '/' do
 	erb :index
@@ -11,12 +36,6 @@ end
 get '/h/:text' do
 	text = params[:text]
 	header 'Content-Type' => 'text/javascript'
-	md5 = Digest::MD5.hexdigest(text)
-	sha1 = Digest::SHA1.hexdigest(text)
-	sha2 = Digest::SHA256.hexdigest(text)
-	body = "
-		$('md5').value = '#{md5}';
-		$('sha1').value = '#{sha1}';
-		$('sha2').value = '#{sha2}';
-	"
+	hashy = Hashy.new
+	hashy.get_hashes('ha$hy')
 end
